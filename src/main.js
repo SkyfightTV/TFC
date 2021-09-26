0af8eed9731e0ec0bd03fee94e1f6135922234a1
SkyfightTV <64917674+SkyfightTV@users.noreply.github.com>
2021-09-27 01:55:14 +0200

    Init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-
-const styles = require('./styles');
 
 export default function App() {
-  return (
-    <View style={styles.styles.container}>
-      <Text>Open up App.js to staon your app!</Text>
-      <Text>Open up App.js to staon your app!</Text>
-    </View>
-  );
+    require('./main/main').start();
 }
---
+++ src/main/main.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+const styles = require('/src/styles');
+
+export function start() {
+    return (
+        <View style={styles.styles.container}>
+            <Text>Open up App.js to staon your app!</Text>
+            <Text>Open up App.js to staon your app!</Text>
+        </View>
+    );
+}
