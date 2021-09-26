1d3ac8693366796d81fbacc239b01f01d073f015
SkyfightTV <64917674+SkyfightTV@users.noreply.github.com>
2021-09-27 01:50:16 +0200

    tg

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
 
@@ -9,7 +8,6 @@ export default function App() {
     <View style={styles.styles.container}>
       <Text>Open up App.js to staon your app!</Text>
       <Text>Open up App.js to staon your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
