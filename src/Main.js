b11b9ac09e866db256c82207398f381b2158e247
SkyfightTV <64917674+SkyfightTV@users.noreply.github.com>
2021-09-27 19:17:15 +0200

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './home/Home'
 import Project from './project/Project'
 import {Image, TouchableOpacity} from "react-native";
+import Styles from "./Styles";
 
 const paramButton =
     <TouchableOpacity>
@@ -12,15 +13,15 @@ const paramButton =
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+function main() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" screenOptions={{headerTitleAlign: 'center', headerRight: () => paramButton}}>
-                <Stack.Screen name="Home" component={Home}/>
-                <Stack.Screen name="Project" component={Project}/>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{headerTitleAlign: 'center', headerStyle: Styles.colors.primary, headerTitleStyle: Styles.colors.text}}>
+                <Stack.Screen name="Home" component={Home} options={{title: "Tarte Flambée Calculator"}}/>
+                <Stack.Screen name="Project" component={Project} options={{title: "Ma nouvelle soirée"}}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
 
-export default App;
+export default main;
