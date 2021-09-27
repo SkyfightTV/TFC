1b031245028bb94b5cdf96b9f3951389ea5d3e7c
SkyfightTV <64917674+SkyfightTV@users.noreply.github.com>
2021-09-27 19:17:22 +0200

    Create Objects.js

---
+++ src/project/objects/Objects.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+export default class Objects extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Salt OOOOO</Text>
+                <Text>Salt OOOOO</Text>
+                <Text>Salt OOOOO</Text>
+                <Text>Salt OOOOO</Text>
+                <Text>Salt OOOOO</Text>
+                <Text>Salt OOOOO</Text>
+            </View>
+        )
+    }
+}
