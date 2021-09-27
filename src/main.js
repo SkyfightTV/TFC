9f8d42290f25ffc2b2504ee157cd8da7ae1b0107
SkyfightTV <64917674+SkyfightTV@users.noreply.github.com>
2021-09-27 02:16:32 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import render from './main/main'
+import {Text, View} from "react-native";
+import {styles} from "./styles";
 
 export default class main extends React.Component {
     render() {
-        render();
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to staon your app!</Text>
+                <Text>Open up App.js to staon your app!</Text>
+            </View>
+        );
     }
 }
